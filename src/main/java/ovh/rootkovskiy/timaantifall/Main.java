8fd259c40b09026d8975c15069a5f67285d52b3f
uwuohayo <im.cheater.xxx@gmail.com>
2022-09-22 12:15:43 +0300

    release

--- src/main/java/ovh/rootkovskiy/timaantifall/Main.java
+++ src/main/java/ovh/rootkovskiy/timaantifall/Main.java
@@ -5,7 +5,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin implements Listener {
@@ -13,15 +12,15 @@ public final class Main extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
 
-        getServer().getPluginManager().registerEvents(this, (Plugin)this);
+        getServer().getPluginManager().registerEvents(this, this);
         getLogger().info("Plugin enabled");
         this.saveDefaultConfig();
-        System.out.println(Console.ANSI_GREEN+"#-#-#-#-#-#-#-#-#"+Console.ANSI_RESET);
-        System.out.println(Console.ANSI_CYAN+   "TimaAntiFall 1.0-release Loaded and Enabled!"+Console.ANSI_RESET);
-        System.out.println(Console.ANSI_CYAN+   "MC Version: "+getServer().getBukkitVersion()+Console.ANSI_RESET);
-        System.out.println(Console.ANSI_CYAN+   "Author: Timur Rootkovskiy (Adminov)"+Console.ANSI_RESET);
-        System.out.println(Console.ANSI_CYAN+   "VK: @timurroot"+Console.ANSI_RESET);
-        System.out.println(Console.ANSI_GREEN+"#-#-#-#-#-#-#-#-#"+Console.ANSI_RESET);
+        System.out.println(Console.ANSI_GREEN + "#-#-#-#-#-#-#-#-#" + Console.ANSI_RESET);
+        System.out.println(Console.ANSI_CYAN + "TimaAntiFall 1.0-release Loaded and Enabled!" + Console.ANSI_RESET);
+        System.out.println(Console.ANSI_CYAN + "MC Version: " + getServer().getBukkitVersion() + Console.ANSI_RESET);
+        System.out.println(Console.ANSI_CYAN + "Author: Timur Rootkovskiy (Adminov)" + Console.ANSI_RESET);
+        System.out.println(Console.ANSI_CYAN + "VK: @timurroot" + Console.ANSI_RESET);
+        System.out.println(Console.ANSI_GREEN + "#-#-#-#-#-#-#-#-#" + Console.ANSI_RESET);
 
     }
 
@@ -29,13 +28,13 @@ public final class Main extends JavaPlugin implements Listener {
     public void onDisable() {
 
         getLogger().info("Plugin disabled");
-        System.out.println(Console.ANSI_GREEN+"#-#-#-#-#-#-#-#-#"+Console.ANSI_RESET);
-        System.out.println(Console.ANSI_CYAN+   "TimaAntiFall 1.0-release Disabled!"+Console.ANSI_RESET);
-        System.out.println(Console.ANSI_CYAN+   "MC Version: "+getServer().getBukkitVersion()+Console.ANSI_RESET);
-        System.out.println(Console.ANSI_CYAN+   "Author: Timur Rootkovskiy (Adminov)"+Console.ANSI_RESET);
-        System.out.println(Console.ANSI_CYAN+   "VK: @timurroot"+Console.ANSI_RESET);
-        System.out.println(Console.ANSI_CYAN+   "Goodbye ;p"+Console.ANSI_RESET);
-        System.out.println(Console.ANSI_GREEN+"#-#-#-#-#-#-#-#-#"+Console.ANSI_RESET);
+        System.out.println(Console.ANSI_GREEN + "#-#-#-#-#-#-#-#-#" + Console.ANSI_RESET);
+        System.out.println(Console.ANSI_CYAN + "TimaAntiFall 1.0-release Disabled!" + Console.ANSI_RESET);
+        System.out.println(Console.ANSI_CYAN + "MC Version: " + getServer().getBukkitVersion() + Console.ANSI_RESET);
+        System.out.println(Console.ANSI_CYAN + "Author: Timur Rootkovskiy (Adminov)" + Console.ANSI_RESET);
+        System.out.println(Console.ANSI_CYAN + "VK: @timurroot" + Console.ANSI_RESET);
+        System.out.println(Console.ANSI_CYAN + "Goodbye ;p" + Console.ANSI_RESET);
+        System.out.println(Console.ANSI_GREEN + "#-#-#-#-#-#-#-#-#" + Console.ANSI_RESET);
 
     }
 
@@ -51,10 +50,10 @@ public final class Main extends JavaPlugin implements Listener {
 
         if (y < getConfig().getInt("MIN_Y") && event.getPlayer().getWorld().getName().equalsIgnoreCase(getConfig().getString("WORLD"))) {
             plr.teleport(spawn);
-            if(title) {
-                plr.sendTitle(getConfig().getString("TITLE").replace("&", "§"), null, 20,40,20);
+            if (title) {
+                plr.sendTitle(getConfig().getString("TITLE").replace("&", "§"), null, 20, 40, 20);
             }
-            if(message){
+            if (message) {
                 plr.sendMessage(getConfig().getString("FALL_MESSAGE").replace("&", "§"));
             }
         }
